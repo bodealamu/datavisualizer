69c6f596160382ecfeb8dbe23deaa886a2535658
olabodealamu <bodealamu007@gmail.com>
2021-05-23 17:52:44 -0500

    added procfile

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn datavisualizer:server
--- datavisualizer.py
+++ datavisualizer.py
@@ -10,7 +10,7 @@ from dash.dependencies import Input, Output
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-
+server = app.server
 
 # callbacks
 @app.callback(
